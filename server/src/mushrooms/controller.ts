bc5ed668a05df199efd0c56cd130db112dfa9c22
kozlowskam <malgorzatta.kozlowska@gmail.com>
2018-07-25 21:39:41 +0200

    universal filter query

--- server/src/mushrooms/controller.ts
+++ server/src/mushrooms/controller.ts
@@ -1,5 +1,7 @@
-import { JsonController, Get, Param } from "routing-controllers";
+import { Context } from "koa";
+import { JsonController, Get, Param, Ctx } from "routing-controllers";
 import Mushroom from "./entity";
+// import { In } from "typeorm";
 
 @JsonController()
 export default class MushroomController {
@@ -8,16 +10,41 @@ export default class MushroomController {
     return Mushroom.findOne(id);
   }
 
+  @Get("/mushrooms/:edible")
+  getMushroomEd(@Param("edible") edible: string) {
+    return Mushroom.findOne(edible);
+  }
+
+  // @Get("/mushrooms?edible=POISONOUS)
+  // async allposion() {
+  //  const loadedPosts = await connection.getRepository(Mushroom).find({
+  //     title: In(["About #2", "About #3"])
+  // });
+  // return {poisoned}
+  // }
+
   @Get("/mushrooms")
-  async allMushrooms() {
-    const mushrooms = await Mushroom.find();
+  async allMushrooms(@Ctx() ctx: Context) {
+    const query = ctx.query;
+    const mushrooms = await Mushroom.find({ where: { ...query } });
     return { mushrooms };
   }
 
-  @Get("/poisonous/")
+  @Get("/poisonous")
   async getPoisones() {
-    const poison = "POISONOUS";
+    const poison = "p";
     const poisones = await Mushroom.find({ where: { edible: poison } });
     return { poisones };
   }
 }
+//http :4000/mushrooms?edible=poisonous
+
+// add BodyParam{}
+
+// @Get("/mushrooms")
+// async allMushrooms(@Ctx() ctx: Context) {
+//   const query = ctx.query;
+//   console.log(query);
+//   const mushrooms = await Mushroom.find({ select: ["edible"] });
+//   return { mushrooms };
+// }
